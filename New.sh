11f56d96e3acc00d66afe2b88013ad8a6a26168e
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-17 20:47:52 +0300

    Update New.sh

--- New.sh
+++ New.sh
@@ -1,2 +1,18 @@
-Echo "Hell World"
-Read x
+echo "enter first value:"
+read x
+echo "enter second value"
+read y
+echo "enter action symbol"
+read act
+ 
+case $act in
+"+") echo " $x + $y ="  $(expr $y + $x);;
+"-") echo "$x - $y ="   $(expr $x - $y);;
+"/") if [ $y -eq 0 ]; then
+       echo "error: delimiter by zero";
+     else
+       echo " $x / $y =" $(expr  $x / $y);
+     fi;;
+"*") echo " $x * $y =" $(expr  $x \* $y);;
+*) echo "command is unknown!"
+esac
