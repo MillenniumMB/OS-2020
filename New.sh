567f7af4bcaa90e469d922f1f6f0151a4d6a2bf7
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-17 22:34:56 +0300

    Update New.sh

--- New.sh
+++ New.sh
@@ -4,14 +4,4 @@ echo "enter second value"
 read y
 echo "enter action symbol"
 read act
- 
-case $act in
-"+") echo " $x + $y ="  $(expr $y + $x);;
-"sub") echo "$x - $y ="   $(expr $x - $y);;
-"mul") if [ $y -eq 0 ]; then
-       echo "error: delimiter by zero";
-     else
-       echo " $x / $y =" $(expr  $x / $y);
-     fi;;
-"div") echo " $x * $y =" $(expr  $x \* $y);;
-esac
+
