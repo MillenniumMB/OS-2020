75976ac699a9fc8fe9ce871faec803a3ddbb547c
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-18 21:40:18 +0300

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,21 +1,13 @@
 #!bin/bash
-function calculator {
-        if [[ $2 =~ "^[0-9]+([.][0-9]+)?$" ]] ; then
-            echo "3 parameter is not a number" >&2
-        elif [[ $3 =~ "^[0-9]+([.][0-9]+)?$" ]] ; then
-            echo "4 parameter is not a number" >&2
-        else
-            if [[ $1 = "sum" ]] ; then
-                echo $(($2 + $3))
-            elif [[ $1 = "sub" ]] ; then
-                echo $(($2 - $3))
-            elif [[ $1 = "mul" ]] ; then
-                echo $(($2 * $3))
-            elif [[ $1 = "div" ]] ; then
-                echo $(($2 / $3))
-            else
-                echo -e "No such command for calculator\nUse sum/sub/mul/div" >&2
-            fi
-        fi
 
-}
+if [[ $1 = "sum" ]] ; then
+     echo $(($2 + $3))
+elif [[ $1 = "sub" ]] ; then
+     echo $(($2 - $3))
+elif [[ $1 = "mul" ]] ; then
+        echo $(($2 * $3))
+elif [[ $1 = "div" ]] ; then
+      echo $(($2 / $3))
+else
+       echo -e "No such command for calculator\nUse sum/sub/mul/div" >&2
+fi
