48f0b3aa39883a357e14d9d0d336837959204b9e
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 14:13:38 +0300

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-printstr(){
-echo "hello world"
-printstr
+  
+function calculator {
+        case $1 in
+        "sum" )
+            echo $(($2 + $3))
+        ;;
+        "sub" )
+            echo $(($2 - $3))
+        ;;
+        "mul" )
+            echo $(($2 * $3))
+        ;;
+        "div" )
+            echo $(($2 / $3))
+        ;;
+        * )
+            echo -e "No such command for calculator\nUse sum/sub/mul/div" >&2
+        ;;
+        esac
 }
-printstr
+calculator
