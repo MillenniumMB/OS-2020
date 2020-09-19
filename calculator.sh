6cdbe9c88d789695f18de170f22819abc2f98350
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 17:15:33 +0300

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,16 +1,20 @@
 function calc {
-        case $2 in
+        if ![[ $2 =~ '^[+-]?[0-256]+$' && $3 =~ '^[+-]?[0-256]+$' ]] then
+                echo "invalid argument"
+                exit 1
+        fi
+        case $1 in
         "sum" )
-            echo $(($3 + $4))
+            echo $(($2 + $3))
         ;;
         "sub" )
-            echo $(($3 - $4))
+            echo $(($2 - $3))
         ;;
         "mul" )
-            echo $(($3 * $4))
+            echo $(($2 * $3))
         ;;
         "div" )
-            echo $(($3 / $4))
+            echo $(($2 / $3))
         ;;
         * )
             echo -e "you should only use these arguments\nUse sum/sub/mul/div" >&2
