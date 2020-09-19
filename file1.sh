21b1e35b5a916beef418f22be256312e43071c60
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 14:59:00 +0300

    Create file1.sh

---
+++ file1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+case $1 in
+"calc" )
+  echo ""
+  . ./calculator.sh
+            calculator $2 $3 $4
+;;
+"search" )
+echo ""
+;;
+"reverse" )
+echo ""
+;;
+"strlen" )
+echo ""
+;;
+"log" )
+echo ""
+;;
+"exit" )
+echo ""
+;;
+"interactive" )
+echo ""
+;;
+esac
