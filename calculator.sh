99cbb5631147209500ee4725a0710b04e606eb99
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 17:27:07 +0300

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,10 +1,10 @@
-function calc {
-        if ![[ $2 =~ '^[+-]?[0-256]+$' && $3 =~ '^[+-]?[0-256]+$' ]] then
-                echo "invalid argument"
-                exit 1
-        fi
-        case $1 in
-        "sum" )
+calc() {
+    if ![[ $2 =~ '^[+-]?[0-256]+$' && $3 =~ '^[+-]?[0-256]+$' ]] then
+        echo "invalid argument"
+        exit 1
+    fi
+    case $1 in
+    "sum" )
             echo $(($2 + $3))
         ;;
         "sub" )
