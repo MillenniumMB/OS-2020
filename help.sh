42099bb8e7debc9659cedaa4c84bbd6d721b1874
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 15:30:52 +0300

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,18 +1,25 @@
 function help {
 case $1 in
-"calc" ) echo ""
+"calc" )
+echo "the calculator function accepts 3 arguments as input: 1 (sum/sub/mul/div) which corresponds to (+ / - / * / /), 2 the remaining arguments are integers"
 ;;
-"search" ) echo ""
+"search" )
+echo ""
 ;;
-"reverse" ) echo ""
+"reverse" ) 
+echo "the application writes the contents of the file with the name specified by argument 2 in reverse order to the file with the name passed by argument 3"
 ;;
-"strlen" ) echo ""
+"strlen" )
+echo "outputs the number of characters of the entered 2 argument"
 ;;
-"log" ) echo ""
+"log" )
+echo ""
 ;;
-"exit" ) echo ""
+"exit" )
+echo "terminate the application with a return code of 2 arguments"
 ;;
-"interactive" ) echo ""
+"interactive" )
+echo ""
 ;;
 esac
 }
