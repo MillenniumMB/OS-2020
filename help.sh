7e1689ae4bd6242a5f03a148150874d826e00455
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 14:50:08 +0300

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,17 @@
+function help {
+case $1 in
+"calc") echo ""
+;;
+"search") echo ""
+;;
+"reverse") echo ""
+;;
+"strlen") echo ""
+;;
+"log") echo ""
+;;
+"exit") echo ""
+;;
+"interactive") echo ""
+;;
+}
