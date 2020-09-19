1b7640f1d511ab1f14c15c908dd6b4ea0f8f669e
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-19 16:34:46 +0300

    Create exit.sh

---
+++ exit.sh
@@ -0,0 +1,17 @@
+function exit {
+re='^[+-]?[0-256]+$'
+if [[ -z $1 ]]
+  then
+    echo "0"
+    exit 0
+elif [[ -n $2 ]]
+  then
+    echo "incorrect number of arguments"
+elif ! [[ $yournumber =~ $re ]]
+  then
+   echo "error: Not a number" >&2;
+   exit 1
+else
+  exit $1
+fi
+}
