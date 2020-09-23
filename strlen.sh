50e44afbdb8e5fb9231e828df17430c506db8aa0
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 23:55:10 +0300

    Update strlen.sh

--- strlen.sh
+++ strlen.sh
@@ -2,7 +2,7 @@ function strlen {
   if [[ -z $1 ]]
     then echo -e "\033[31mincorrect number of arguments.\nYou must have 1 argument\033[0m"
     exit 1
-  elif [[ -n $2 ]]
+  elif ! [[ -z $2 ]]
     then echo -e "\033[31mincorrect number of arguments\033[0m"
     exit 1
   fi
