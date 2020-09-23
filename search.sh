5e526c1ada7f93c9e86face8c65659ba3d59886a
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 18:35:18 +0300

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,13 +1,18 @@
 function search {
   if [[ -r $1 ]]
-  then echo "incorrect number of arguments"
+  then echo "\033[31mincorrect number of arguments\033[0m"
     exit 1
   elif [[ -r $2 ]]
-  then echo "incorrect number of arguments"
+  then echo "\033[31mincorrect number of arguments\033[0m"
     exit 1
   fi
    if ! [[ -d $1 ]] ; then
-     echo "\033[31mОшибка - такой директории не существует\033[0m"
+     echo "\033[31mError:not faind dirrectory not found\033[0m"
       exit 1
+   elif ! [[ -r $1 ]]
+    then
+      echo -e "\033[31mYou will not be able to open this diretoria $1\033[0m"
+      exit -1
     fi
+    grep -r -s "$2" "$1"    
 }
