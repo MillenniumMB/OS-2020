4505bdbaa53b05f7d5ac59f41733c0c51a6086b7
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 00:01:38 +0300

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,34 +1,58 @@
 #!/bin/bash
 case $1 in
 "calc" )
+elif ! [[ -z $5 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./calc.sh
             calc $2 $3 $4
 ;;
 "search" )
+elif ! [[ -z $4 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./search.sh
             search $2 $3
 ;;
 "reverse" )
+elif ! [[ -z $4 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./reverse.sh
             reverse $2 $3
 ;;
 "strlen" )
+elif ! [[ -z $3 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./strlen.sh
-            strlen $2 $3
+            strlen $2
 ;;
 "log" )
   echo ""
 ;;
 "exit" )
+elif ! [[ -z $3 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./exit.sh
             exit_ $2
 ;;
 "help" )
+elif ! [[ -z $3 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
   echo ""
   . ./help.sh
             help $2
