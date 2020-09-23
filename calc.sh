eae5f611fb1defcee748e4ef17784e57559537fb
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 19:25:49 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -2,7 +2,7 @@ function calc {
     if [[ -z $1 || -z $2 || -r $3 ]]
   then echo -e "\033[31mincorrect number of arguments\033[0m"
   exit 1
-  elif ![[ $2 =~ '^[+-]?[0-256]+$' && $3 =~ '^[+-]?[0-256]+$' ]]
+  elif ! [[ $2 =~ '^-?[0-256]+$' && $3 =~ '^-?[0-256]+$' ]]
   then echo -e "\033[31you must enter 2 numbers after sum/sub/mul/div\033[0m"
   fi
     case $1 in
