5056e6cfc0ff61716199cd0c6886dd41748a297f
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 23:27:17 +0300

    Create strlen.sh

---
+++ strlen.sh
@@ -0,0 +1,10 @@
+function strlen {
+  if [[ -z $1 ]]
+    then echo -e "\033[31mincorrect number of arguments.\nYou must have 1 argument\033[0m"
+    exit 1
+  elif [[ -n $2 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  fi
+  echo ${#$1}
+}
