55e7798c0e3602da931ca81d7c7f624d6a075953
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 16:37:51 +0300

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,11 +1,11 @@
 function search {
-  if [[ -r $1 ]] then
-    echo "incorrect number of arguments"
+  if [[ -r $1 ]]
+  then echo "incorrect number of arguments"
     exit 1
-  elif [[ -r $2 ]] then
-    echo "incorrect number of arguments"
+  elif [[ -r $2 ]]
+  then echo "incorrect number of arguments"
     exit 1
-   fi
+  fi
    if ! [[ -d $1 ]] ; then
      echo -e "\033[31mОшибка - такой директории не существует\033[0m"
       noDirectoryError
