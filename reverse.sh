a7cd119947f27e88a1a939bca948e9ffe1e7c359
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-23 21:44:16 +0300

    Create reverse.sh

---
+++ reverse.sh
@@ -0,0 +1,18 @@
+function reverse {
+  if [[ -z $1 || -z $2 ]]
+    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    exit 1
+  elif ! [[ -r $1 || -r $2 ]]
+    then echo -e "\033[31you can't use this file\033[0m"
+    exit 1
+  elif [[ -d $1 || -d $2 ]]
+    then echo -e "\033[31mError:it is directory\033[0m"
+      exit 1
+  elif ! [[ -f $1 ]]
+    then echo -e "\033[31file not found\033[0m"
+  fi
+  if ! [[ -f $2 ]]
+    then sudo chmod +x "$2"
+  fi
+  tac $1 | rev > $2
+}
