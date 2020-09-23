49b9dc6c0388023fe0e5ad416e09d82d324d7047
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 00:37:32 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -5,11 +5,11 @@ function log {
     if [[ "$line" = *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]
     then echo -e "${line//"(WW)"/"\033[33mWarning:\033[0m"}"
     fi
-  done < file1
+  done < "$file1"
   while read line
   do
     if [[ "$line" = *"(II)"* && "$line" != *"(++) from command line, (!!) notice, (II) informational,"* ]]
     then echo -e "${line//"(II)"/"\033[94mInformation:\033[0m"}"
     fi
-  done < file1
+  done < "$file1"
 }
