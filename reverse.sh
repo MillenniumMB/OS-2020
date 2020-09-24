a8bc0e1e508da7eab3ab2bde5321248021e3d424
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 19:17:33 +0300

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -1,23 +1,23 @@
 function reverse {
   if [[ -z $1 || -z $2 || -n $3 ]] && ! [[ "$3" = "interactive" ]]
-    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    then echo -e "\033[31mIncorrect number of arguments\033[0m"
     exit 1
   elif ! [[ -r $1 || -r $2 ]]
-    then echo -e "\033[31you can't use this file\033[0m"
+    then echo -e "\033[31mYou can't use this file\033[0m"
     if [[ "$3" = "interactive" ]]
         then return -2
     else
         exit -2
     fi
   elif [[ -d $1 || -d $2 ]]
-    then echo -e "\033[31merror:it is directory\033[0m"
+    then echo -e "\033[31mError:it is directory\033[0m"
        if [[ "$3" = "interactive" ]]
         then return -2
     else
         exit -2
     fi 
   elif ! [[ -f $1 ]]
-    then echo -e "\033[31file not found\033[0m"
+    then echo -e "\033[31mFile not found\033[0m"
      if [[ "$3" = "interactive" ]]
         then return -2
     else
