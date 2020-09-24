00f0465b7e30074dcb7c3c74df68b13010ae3dd8
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 13:35:21 +0300

    Update interactive.sh

--- interactive.sh
+++ interactive.sh
@@ -1,12 +1,14 @@
 function File_Error {
 if ! [[ -e $1.sh ]]
   then
-    echo -e "\033[31mError not file $1.sh\033[0m"
+    echo -e "\033[31mError not file $1.sh
+    you can't use this function\033[0m"
     return -1
   fi
   if ! [[ -r $1.sh ]]
   then
-    echo -e "\033[31mError insufficient rights $1.sh\033[0m"
+    echo -e "\033[31mError insufficient rights $1.sh
+    you can't use this function\033[0m"
     return -2
   fi
   return 0
@@ -14,31 +16,43 @@ if ! [[ -e $1.sh ]]
 function interactive {
 case $1 in
 "a")
-File_Error "cal"
+File_Error "calc"
+echo -e "Write the sum command sum/sub/mul/div"
+read command
+echo -e "Write the first number"
+read number1
+echo -e "Write the second number"
+read number2
 . ./calc.sh
 
-            calc sum 4 5
+            calc $command $number1 $number2 "interactive"
 ;;"b")
+File_Error "search"
 . ./search.sh
             search
 ;;
 "c")
+File_Error "reverse"
 . ./reverse.sh
             reverse
 ;;
 "d")
+File_Error "strlen"
 . ./strlen.sh
             strlen
 ;;
 "e")
+File_Error "log"
 . ./log.sh
             log
 ;;
 "f")
+File_Error "exit"
 . ./exit.sh
             exit_
 ;;
 "g")
+File_Error "help"
 . ./help.sh
             help
 ;;
