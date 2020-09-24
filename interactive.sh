7ed7c6aba074ef5710d64b680e35afb282663686
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 11:13:13 +0300

    Update interactive.sh

--- interactive.sh
+++ interactive.sh
@@ -1,1 +1,23 @@
+functuon Back_To_Menu {
+while true
+  do echo -e "\033[94menter one of the commands\033[0m:
+                a: calculator
+                b: search
+                c: reverse
+                d: strlen
+                e: log
+                f: exit
+                g: help"
+        read VAR
+        interactive $VAR
+    done
+}
 
+functuon interactive {
+case $1 in
+"f")
+. ./exit.sh
+            exit_
+;;
+esac
+}
