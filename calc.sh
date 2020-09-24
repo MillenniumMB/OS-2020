0ea44848fefc58f0ed408cd60436da14041fde4d
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 13:59:03 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,11 +1,14 @@
 function calc {
-    if [[ -z $1 || -z $2 || -r $3 ]] && ! [[ "$3" = "interactive" ]]
+    if [[ -z $1 || -z $2 || -z $3 || -n $4 ]] && ! [[ "$4" = "interactive" ]]
   then echo -e "\033[31mincorrect number of arguments\033[0m"
-  exit 1
+  exit -3
   elif ! [[ $2 =~ ^[+-]?[0-9]+$ && $3 =~ ^[+-]?[0-9]+$ ]]
   then
   echo -e "\033[31myou must enter 2 numbers after div/sum/mul/div\033[0m"
-  exit 1
+  if [[ "$4" = "interactive" ]]
+        then return -4
+  else
+    exit -4
   fi
     case $1 in
     "sum" )
@@ -18,10 +21,13 @@ function calc {
             echo $(($2 * $3))
         ;;
         "div" )
-        if [[ $4 -eq 0 || $4 -eq -0 || $4 -eq +0 ]]
+        if [[ $3 -eq 0 || $3 -eq -0 || $3 -eq +0 ]]
         then
         echo -e "\033[31mError division by zero\033[0m"
-        exit 1
+        if [[ "$4" = "interactive" ]]
+            then return -1
+        else
+            exit -1
         fi
             echo $(($2 / $3))
         ;;
