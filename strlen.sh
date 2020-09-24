4793507564707475c3e5ea67ee119b0b32783b69
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 20:36:44 +0300

    Update strlen.sh

--- strlen.sh
+++ strlen.sh
@@ -1,6 +1,6 @@
 function strlen {
   if [[ -z $1 || -n $2 ]] && ! [[ "$2" = "interactive" ]]
-    then echo -e "\033[31mincorrect number of arguments.\nYou must have 1 argument\033[0m"
+    then echo -e "\033[31mincorrect number of arguments.\nYou must have 1 argument\033[0m" >&2
     exit -3
   fi
   VAR="$1"
