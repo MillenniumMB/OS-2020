ec40a602361c8a4dc91fa1fb5036e557f87dd8c5
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 20:00:42 +0300

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -7,11 +7,11 @@ if [[ -z $1 || "$1" = "interactive" ]]
     exit 0
 elif [[ -n $2 ]] && ! [[ "$2" = "interactive" ]]
   then
-    echo -e "\033[31mincorrect number of arguments\033[0m"
+    echo -e "\033[31mincorrect number of arguments\033[0m" >&2
     exit -3
 elif ! [[ $1 =~ $re ]]
   then
-   echo -e "\033[31merror: Not a number\033[0m"
+   echo -e "\033[31merror: Not a number\033[0m" >&2
    if [[ "$2" = "interactive" ]]
         then return -4
     else
