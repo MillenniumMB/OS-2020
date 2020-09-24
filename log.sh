89583f954d2b3d70e6c6a0f6277f2a4083fff63d
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 20:28:43 +0300

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,8 +1,15 @@
 function log {
   if [[ -n $1 ]] && ! [[ "$1" = "interactive" ]]
-    then echo -e "\033[31mincorrect number of arguments\033[0m"
+    then echo -e "\033[31mincorrect number of arguments\033[0m" >&2
     exit -3
-  fi
+  elif ! [[ -r "/var/log/anaconda/X.log" ]]
+    then
+      echo -e "\033[31mYou will not be able to open this diretoria $1\033[0m" > &2
+      if [[ "$1" = "interactive" ]]
+        then return -2
+      else
+        exit -2
+      fi
   while read line
     do
     if [[ "$line" = *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]
