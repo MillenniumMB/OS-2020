69409948945fcda2b8f0ee5c76338aee6ba249f4
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 19:58:25 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -3,10 +3,10 @@ return 0
 }
 function calc {
     if [[ -z $1 || -z $2 || -z $3 || -n $4 ]] && ! [[ "$4" = "interactive" ]]
-  then echo -e "\033[31mincorrect number of arguments\033[0m"
+  then echo -e "\033[31mincorrect number of arguments\033[0m" >&2
   exit -3
   elif ! [[ $2 =~ ^[+-]?[0-9]+$ && $3 =~ ^[+-]?[0-9]+$ ]]
-    then echo -e "\033[31myou must enter 2 numbers after div/sum/mul/div\033[0m"
+    then echo -e "\033[31myou must enter 2 numbers after div/sum/mul/div\033[0m" >&2
     if [[ "$4" = "interactive" ]]
         then return -4
     else
@@ -25,7 +25,7 @@ function calc {
         ;;
         "div" )
         if [[ $3 -eq 0 || $3 -eq -0 || $3 -eq +0 ]]
-        then echo -e "\033[31mError division by zero\033[0m"
+        then echo -e "\033[31mError division by zero\033[0m" >&2
             if [[ "$4" = "interactive" ]]
                 then 
                 return -1
@@ -36,7 +36,7 @@ function calc {
             echo $(($2 / $3))
         ;;
         * )
-            echo -e "you should only use these arguments\nUse sum/sub/mul/div"
+            echo -e "\033[31myou should only use these arguments\nUse sum/sub/mul/div\033[0m" >&2
         ;;
         esac
 }
