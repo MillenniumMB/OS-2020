70ea6d418eb593d68b6c55e23280a9f0510813da
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-24 19:30:38 +0300

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,18 +1,26 @@
 function search {
-  if [[ -z $1 ]]
+  if [[ -z $1  || -n $3]] && ! [[ "$3" = "interactive" ]]
   then echo -e "\033[31mincorrect number of arguments\033[0m"
-    exit 1
+    exit -3
   elif [[ -z $2 ]]
   then echo -e "\033[31mincorrect number of arguments\033[0m"
-    exit 1
+    exit -3
   fi
    if ! [[ -d $1 ]] ; then
      echo -e "\033[31mError:not faind dirrectory not found\033[0m"
-      exit 1
+     if [[ "$3" = "interactive" ]]
+        then return -2
+     else
+        exit -2
+     fi
    elif ! [[ -r $1 ]]
     then
       echo -e "\033[31mYou will not be able to open this diretoria $1\033[0m"
-      exit -1
+      if [[ "$3" = "interactive" ]]
+        then return -2
+      else
+        exit -2
+      fi
     fi
     grep -r -s "$2" "$1"    
 }
