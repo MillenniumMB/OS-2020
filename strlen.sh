200a868f111727a616d9ea5693850081d72b3bfe
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-09-30 16:53:18 +0300

    Update strlen.sh

--- strlen.sh
+++ strlen.sh
@@ -1,12 +1,4 @@
 function strlen {
-  if [[ -z $1 || -n $2 ]] && ! [[ "$2" = "interactive" ]]
-    then echo -e "\033[31mincorrect number of arguments.\nYou must have 1 argument\033[0m" >&2
-    if [[ "$2" = "interactive" || "$3" = "interactive" ]]
-        then return -3
-    else
-        exit -3
-    fi
-  fi
   VAR="$1"
   size=${#VAR}
   echo -e "Len: $size"
