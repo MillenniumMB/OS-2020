7582042d9d79dc54d519dc8b21e9c92ce63d4f7d
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-10-20 21:32:59 +0300

    Update task1.sh

--- Lab2/task1.sh
+++ Lab2/task1.sh
@@ -4,5 +4,5 @@ proc=$(ps -u user | tail -n +2 | awk '{print $1":"$4}')
 count_proc=$(echo "$proc" | wc -l)
 
 
-echo "Количество процессов: $count_proc\n" > new.txt
-echo -e "$proc" >> new.txt
+echo "Количество процессов: $count_proc\n" > file.txt
+echo -e "$proc" >> file.txt
