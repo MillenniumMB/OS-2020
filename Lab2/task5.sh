4860778daf3610bb31b53199cc2b7d120cdfaa8c
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-10-22 19:08:07 +0300

    Create task5.sh

---
+++ Lab2/task5.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+sum=0
+previousPPID=-1
+count=0
+
+while read line
+do
+
+ppid=$(echo $line | cut -d ":" -f 2 | grep -E -s -o "[0-9]+")
+runTime=$(echo $line | cut -d ":" -f 3 | grep -E -s -o "[0-9]+")
+
+if (( previousPPID != -1 && ppid != previousPPID ))
+then
+echo "Average_Sleeping_children_of_ParentID=$ppid is" $(echo "scale=2; $sum / $count" | bc -l)
+sum=0
+count=0
+fi
+
+sum=$(echo "scale=2; $sum + $runTime" | bc -l)
+previousPPID=$ppid
+count=$(($count+1))
+
+done < task4.txt > task5.txt
+
+echo "Average_Sleeping_children_of_ParentID=$previousPPID is" $(echo "scale=2; $sum / $count" | bc -l) >> task5.txt
