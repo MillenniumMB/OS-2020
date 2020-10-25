be37c421d6ac5490e324256e99137be52dabe6db
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-10-26 00:12:04 +0300

    Update task6.sh

--- Lab2/task6.sh
+++ Lab2/task6.sh
@@ -1,22 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-max_pid=-1
-max_size=-1
+max="0"
 
-for pid in $(ps -A o pid)
-do
-currentSize=$(grep -s "VmSize" "/proc/"$pid"/status" | grep -E -o "[0-9]+")
-if [[ $currentSize -gt $max_size ]]
-then
-max_pid=$pid
-max_size=$currentSize
-fi
+for pid in $(ps uax | awk 'FNR > 1 { print $2 } '); do	
+  if [[ -r /proc/$pid/status ]]; then
+	  a="$(grep -i "VMRSS" /proc/$pid/status | awk '{ print $2 }')"
+	  if [[ "$a" -ne "" ]]; then
+      if [[ "$a" -gt "$max" ]]; then
+        ans=$pid
+        max=$a
+	    fi
+    fi
+   fi
 done
 
-top -p $max_pid -b -n 1 > task6.txt
-
-vmSize=$(cat "task6.txt" | grep $max_pid | awk '{print $5}')
-
-echo "pid VmSize:$max_pid"
-echo "mem in /proc $max_size"
-echo "mem in top $vmSize"
+echo "Process_PID: "$ans" Memory: "$max
