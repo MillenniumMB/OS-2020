4498136f713173a1f64cff5f755b767159150538
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-10-26 00:22:52 +0300

    Update task7.sh

--- Lab2/task7.sh
+++ Lab2/task7.sh
@@ -1,27 +1,24 @@
-  #!/usr/bin/bash
+#!/bin/bash
 
-for i in $(ps -A o pid,command | awk '{print $1":"$2}')
-do
-pid=$(echo $i | cut -d ":" -f 1)
-cmd=$(echo $i | cut -d ":" -f 2)
-past_read_bytes=$(grep -s "read_bytes" "/proc/"$pid"/io" | grep -E -o "[0-9]+")
-if [[ -n $past_read_bytes ]]
-then
-echo "$pid $cmd $past_read_bytes"
-fi
-done > "task7.txt"
+file_before_temp="2_7_1.temp"
+file_after_temp="2_7_2.temp"
 
+ps aux | awk '{if ($6 != 0) print $2" "$6" "$11}' | sort -nk1 | tail -n +2 | head -n -6 > "$file_before_temp"
 sleep 1m
-for i in $(ps -A o pid,command | awk '{print $1":"$2}' )
-do
-pid=$(echo $i | cut -d ":" -f 1)
-cmd=$(echo $i | cut -d ":" -f 2)
-new_read_bytes=$(grep -s "read_bytes" "/proc/"$pid"/io" | grep -E -o "[0-9]+")
-past_read_bytes=$(cat "task7.txt" | grep "$pid" | awk '{print $3}')
-if [[ -n $past_read_bytes && -n $new_read_bytes ]]
-then
-read_bytes=$(($new_read_bytes - $past_read_bytes))
-echo "PID:$pid CMD:$cmd BYTES:$(( $read_bytes ))"
-fi
-done | sort -n -t ':' -k 3 | head -3
+ps aux | awk '{if ($6 != 0) print $2" "$6" "$11}' | sort -nk1 | tail -n +2 | head -n -6 > "$file_after_temp"
 
+cat "$file_before_temp" | 
+while read line
+do
+    pid=$(awk '{print $1}' <<< $line)
+    mem_before=$(awk '{print $2}' <<< $line)
+    cmd=$(awk '{print $3}' <<< $line)
+    
+    mem_after=$(cat "$file_after_temp" | awk -v id="$pid" '{if ($1 == id) print $2}')
+    mem_delta=$(echo "$mem_after-$mem_before" | bc)
+    
+    echo $pid":"$cmd":"$mem_delta
+    
+done | sort -t ":" -nk3 | head -n 3
+
+rm *.temp
