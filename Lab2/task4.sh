c1a73c691441c1be50cd619461d91000698a50f7
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-10-26 00:06:17 +0300

    Update task4.sh

--- Lab2/task4.sh
+++ Lab2/task4.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 for pid in $(ps -A o pid)
 do
 
-ppid=$(grep -s "PPid" "/proc/"$pid"/status" | grep -E -o "[0-9]+")
-sum_exec_runtime=$(grep -s "sum_exec_runtime" "/proc/"$pid"/sched" | grep -E -o "[0-9]+(.[0-9]+)?")
-nr_switches=$(grep -s "nr_switches" "/proc/"$pid"/sched" | grep -E -o "[0-9]+")
+    file_status="/proc/"$pid"/status"
+    file_sched="/proc/"$pid"/sched"
 
-if [[ -n $nr_switches ]]
-then
-art=$(echo "scale=2; $sum_exec_runtime / $nr_switches" | bc -l)
-echo "ProcessID=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$art"
-fi
+    ppid=$(grep -s "PPid" "$file_status" | grep -E -o "[0-9]+")
+    sumExecRuntime=$(grep -s "sum_exec_runtime" "$file_sched" | grep -E -o "[0-9]+(.[0-9]+)?")
+    nrSwithes=$(grep -s "nr_switches" "$file_sched" | grep -E -o "[0-9]+")
+
+    if [[ -n $nrSwithes ]]
+    then
+        art=$(echo "scale=2; $sumExecRuntime / $nrSwithes" | bc -l)
+        echo "ProcessID=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$art"
+    fi
 done | sort -n -t = -k 2 > task4.txt
