1b491497b4eb348393bf95e3c013f95b0048008c
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-12-03 21:59:44 +0300

    Update task4.sh

--- Lab3/task4.sh
+++ Lab3/task4.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-nice -n 20 ./inf.sh&
-nice -n 0 ./inf.sh&
-nice -n 0 ./inf.sh&
+
+sh inf.sh&res1=$!
+sh inf.sh&res2=$!
+sh inf.sh&res3=$!
+
+renice +10 -p $res1
+
+at now +1 minute <<< "kill ${res3}"
+at now +5 minute <<< "kill ${res1} ; kill ${res2}"
