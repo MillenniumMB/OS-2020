3e2e08632ba8a179eeb03013dd94ba6bb1ccb6fe
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-12-03 17:54:21 +0300

    Create resolver.sh

---
+++ Lab3/resolver.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo $$>mypid
+res=1
+operator='+'
+sigterm()
+{
+exit
+}
+trap 'sigterm' SIGTERM
+
+tail -f input | while true
+do
+read line
+case $line in
+'+')
+operator='+'
+echo "Sum"
+;;
+'*')
+operator='*'
+echo "Product"
+;;
+[0-9]*)
+if [[ $operation == '+' ]]
+then
+let res=$res+$line
+else
+let res=$res*$line
+fi
+echo "$operation$line = $res"
+;;
+echo "ERR"
+;;
+*)
+echo "ERR"
+;;
+esac
+done
