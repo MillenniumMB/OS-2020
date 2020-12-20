f808f64503eadeb08ae8ebfe8ec76ddc5d7981ac
Milena Olegovna <71442442+MillenniumMB@users.noreply.github.com>
2020-12-20 20:17:10 +0300

    Create upback.sh

---
+++ lab4/upback.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/user
+dateFiles="$(ls | grep -E "^Backup-" | sort -n | tail -1  | cut -d "-" -f 2,3,4)"
+if [[ ! -d restore ]]
+then
+mkdir restore
+fi
+cd "Backup-$dateFiles"
+listInBackup="$(ls)"
+for i in $listInBackup
+do
+	filesNotToCopy="$(echo "$i" | grep -a -oP '[A-Za-z0-9]{1,}.[0-9]{2}-[0-9]{2}')"
+	if [ "$i" != "$filesNotToCopy" ]
+	then
+		cp -R "$i" $HOME/restore
+	fi
+done
